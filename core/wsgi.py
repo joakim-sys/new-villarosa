0f312fa5eee1b50a97c20ecbf65972f005443f52
joakim-sys <joachmarvi@gmail.com>
2025-02-06 19:01:54 +0300

    asdf

--- core/wsgi.py
+++ core/wsgi.py
@@ -7,6 +7,6 @@ from django.core.wsgi import get_wsgi_application
 
 dotenv.load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.production")
 
 application = get_wsgi_application()
